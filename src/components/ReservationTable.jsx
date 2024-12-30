f602d2158910ae8f308c86656834c07162558f96
asinghka <singhkalra.arjun@gmail.com>
2024-12-30 03:12:48 +0100

    current arriving reservations are highlighted green & sum is displayed

--- src/components/ReservationTable.jsx
+++ src/components/ReservationTable.jsx
@@ -1,5 +1,5 @@
 import {Table} from "react-bootstrap";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ReservationModal from "./ReservationModal.jsx";
 
 function ReservationTable({fetchReservations, reservations, filterToday = false, filterDate, showModal, setShowModal, admin = false}) {
@@ -17,20 +17,30 @@ function ReservationTable({fetchReservations, reservations, filterToday = false,
         setShowModal(true);
     }
 
-    const isPastReservation = (reservation) => {
+    const isCurrentReservations = (reservation) => {
         if (!filterToday) return false;
 
         const reservationTime = new Date(reservation.date);
         const now = new Date();
 
-        return reservationTime < now.setMinutes(now.getMinutes() - 15);
+        return reservationTime > now.setMinutes(now.getMinutes() - 15) && reservationTime < now.setMinutes(now.getMinutes() + 15);
     };
+
+    const sumPeople = () => {
+        let sum = 0;
+
+        for (const reservation of reservations) {
+            sum += reservation.count;
+        }
+
+        return sum;
+    }
 
     return (
         <>
             <Table bordered hover>
                 <thead>
-                <tr>
+                <tr className="table-secondary">
                     <th>Name</th>
                     <th>Datum</th>
                     <th>Uhrzeit</th>
@@ -39,10 +49,10 @@ function ReservationTable({fetchReservations, reservations, filterToday = false,
                 </thead>
                 <tbody>
                 {reservations.map((reservation) => {
-                    const passed = isPastReservation(reservation);
+                    const current = isCurrentReservations(reservation);
 
                     return (
-                        <tr className={passed ? "table-secondary" : ""} key={reservation.id} onClick={() => handleRowClick(reservation)} style={{ cursor: 'pointer' }}>
+                        <tr className={current ? "table-success" : ""} key={reservation.id} onClick={() => handleRowClick(reservation)} style={{ cursor: 'pointer' }}>
                             <td>{reservation.name}</td>
                             <td>
                                 {new Date(reservation.date).toLocaleDateString('de-DE')}
@@ -54,6 +64,14 @@ function ReservationTable({fetchReservations, reservations, filterToday = false,
                         </tr>
                     );
                 })}
+                {!admin && reservations.length > 0 &&
+                    <tr className="table-secondary">
+                        <td/>
+                        <td/>
+                        <th>Summe Personen</th>
+                        <th>{sumPeople()}</th>
+                    </tr>
+                }
                 </tbody>
             </Table>
             <ReservationModal
