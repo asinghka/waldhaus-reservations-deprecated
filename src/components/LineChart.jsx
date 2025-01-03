a2be71c48c2b03c61d8d5c81b3b15be4fda2299d
asinghka <singhkalra.arjun@gmail.com>
2025-01-03 23:46:27 +0100

    changed linechart styling

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -96,10 +96,39 @@ function LineChart({filterDate = new Date()}) {
             y: {
                 beginAtZero: true,
                 min: 0,
-                max: maxDataValue ? Math.ceil(maxDataValue * 1.2) : 10,
+                max: maxDataValue ? Math.ceil(Math.ceil(maxDataValue * 1.1) / 5) * 5 : 10,
                 ticks: {
-                    stepSize: 1,
+                    stepSize: 5,
+                },
+                grid: {
+                    color: function (context) {
+                        if (context.tick.value === 15) {
+                            return "rgb(182,182,0)";
+                        } else if (context.tick.value === 20) {
+                            return "rgb(195,0,0)";
+                        } else {
+                            return "rgb(0, 0, 0)"
+                        }
+                    },
+                    lineWidth: function (context) {
+                        if (context.tick.value === 15) {
+                            return 2;
+                        } else if (context.tick.value === 20) {
+                            return 2;
+                        } else {
+                            return 1
+                        }
+                    }
                 }
+            },
+            x: {
+                grid: {
+                    drawOnChartArea: true,  // Disables vertical grid lines in the chart area
+                    drawTicks: true,        // Keeps the ticks and their labels visible
+                },
+                ticks: {
+                    display: true,          // Ensures the labels are displayed
+                },
             },
         },
         onClick: () => {
